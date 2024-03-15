b21c03b31afd9b26147adb0cfb35509fc0f55446
Luan <126830757+Luan-Cristian@users.noreply.github.com>
2024-03-14 23:40:56 -0300

    Update testeclient.java

--- testeclient.java
+++ testeclient.java
@@ -4,7 +4,7 @@ package br.com.Caelum.contas;
 public class TesteCliente {
     public static void main(String[] args) {
         // Criar um novo cliente
-        Cliente cliente = new Cliente("tony", "Rua Principal");
+        Cliente cliente = new Cliente("Luan", "Rua Principal");
 
         // Exibir informações do cliente
         cliente.exibirInformacoes();
